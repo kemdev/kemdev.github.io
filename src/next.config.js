b4b0809ce8118fda99e7f5aca5c22085c2ace7fe
kemdev <kareem_kemo@live.com>
2022-07-20 19:52:00 +0200

    .git

--- src/next.config.js
+++ src/next.config.js
@@ -2,6 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/kemdev/kemdev.github.io/gh-pages/' : '',
 }
