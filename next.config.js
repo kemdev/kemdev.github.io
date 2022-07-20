fc1f5873ee7ba46659e1d0b5c4f1d4d2e697e7b3
kemdev <kareem_kemo@live.com>
2022-07-20 19:06:24 +0200

    Changed nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['static1.makeuseofimages.com'],
+//   },
+// }
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['static1.makeuseofimages.com'],
-  },
+  // Use the CDN in production and localhost for development.
+  
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/kemdev/kemdev.github.io/gh-pages/' : '',
 }
