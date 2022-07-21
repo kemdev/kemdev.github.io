35668643eb6764730f66cf86ad84fd8acc21f6fd
kemdev <kemdev@users.noreply.github.com>
2022-07-21 23:32:01 +0000

    deploy: 9a753fea6394989a067114f82b3c2ca687305979

--- README.md
+++
@@ -1,1 +0,0 @@
-# kemdev.github.io
---
+++ _next/static/nHwmw12YdB-vveRCJ64rN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29-b17115cb854087ab.js","static/css/8b0ef973b3082788.css","static/chunks/pages/index-5d96b346a31e8ae3.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
