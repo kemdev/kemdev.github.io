1287800374a3ce0039ecd3040a9da7d9395ea0ac
kemdev <kareem_kemo@live.com>
2022-07-22 01:38:20 +0200

    sd

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  reactStrictMode: false,
-  assetPrefix: isProd ? "kemdev.github.io" : "",
+  assetPrefix: isProd ? "/kemdev.github.io/" : "",
   // distDir: 'src',
   images: {
     domains: ["static1.makeuseofimages.com"],
