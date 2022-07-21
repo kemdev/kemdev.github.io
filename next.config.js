7d056852d7d1098d70e84b3392d03de1f622244c
kemdev <kareem_kemo@live.com>
2022-07-21 14:25:41 +0200

    remove out folder, add cdn to next.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: false,
-  distDir: 'src',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
   images: {
     domains: ['static1.makeuseofimages.com'],
   },
