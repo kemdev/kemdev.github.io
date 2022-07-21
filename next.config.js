7d22bf2704aa7c0b078092c3f424031b32439454
kemdev <kareem_kemo@live.com>
2022-07-21 15:16:24 +0200

    fixed deploy issue

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: false,
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+  distDir: 'src',
   images: {
     domains: ['static1.makeuseofimages.com'],
   },
