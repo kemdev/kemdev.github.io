77c69a3e2502be7eb0379276ed01fd0fb5a04d46
kemdev <kareem_kemo@live.com>
2022-07-21 13:25:24 +0200

    update stuff

---
+++ out/_next/static/sTFTGVqUKSqUueXZOqDUV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29-9b95d4fcb2550b88.js","static/css/8b0ef973b3082788.css","static/chunks/pages/index-d94ed497a169a645.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
